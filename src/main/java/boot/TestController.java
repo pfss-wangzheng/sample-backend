d975d02be051e1199d60605b47272b2f95965357
pfss-wangzheng <wangzheng13@cn.panasonic.com>
2024-11-08 11:24:49 +0800

    test

--- src/main/java/boot/TestController.java
+++ src/main/java/boot/TestController.java
@@ -1,12 +1,14 @@
 package boot;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TestController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    @GetMapping("/hellow")
+    public String hellow() {
+        return "Hellow, World!";
     }
 }
